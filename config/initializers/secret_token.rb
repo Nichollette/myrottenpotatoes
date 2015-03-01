0e1bc8bbec6a1a796af6732bf4521029ad48a11c
Nichollette Acosta <nichollettetaylor@gmail.com>
2015-03-01 03:37:11 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '450504438d5135e1f50e64c8018af5436987fc9d9d13d619fb17ae01e39a1ddbd1ccb21b35f6bac092188c1264daa238080ab287dcd0167c74ae932e2c9b0ce2'
